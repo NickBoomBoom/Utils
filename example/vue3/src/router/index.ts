52fc15877a92ba2926fb17d97530582d6fd26b64
q.chen <q.chen.self@outlook.com>
2021-03-17 15:33:29 +0800

    add 新增example;push限定文件

---
+++ example/vue3/src/router/index.ts
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+
+const routerHistory = createWebHistory()
+
+const router = createRouter({
+  history: routerHistory,
+  routes: [
+    {
+      path: '/',
+      component: Home
+    }
+  ]
+})
+
+export default router
---
+++ example/vue3/src/views/Home.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+.home home
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Home",
+});
+</script>
