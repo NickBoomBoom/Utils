65bf1aedaaa11bd3a99bb46876c7b92fa8959032
q.chen <q.chen.self@outlook.com>
2021-07-16 18:35:51 +0800

    add 月份添加字段

--- src/models/date.model.ts
+++ src/models/date.model.ts
@@ -1,8 +1,11 @@
 export interface Day {
   date: Date;
   data: {
-    day: string;  // 当前日期 2020/10/10
+    year: number;
+    month: number;
+    day: number;
+    text: string; // 当前日期 2020/10/10
     week: number; // 0-6
-    current: boolean // 是否当前月
-  }
+    current: boolean; // 是否当前月
+  };
 }
