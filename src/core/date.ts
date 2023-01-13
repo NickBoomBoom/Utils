a813c564b763e6eac83b7d0c01ba5120779f5d33
q.chen.work <q.chen.work@outlook.com>
2023-01-13 11:13:42 +0800

    update

--- src/core/date.ts
+++ src/core/date.ts
@@ -1,5 +1,5 @@
 import { sliceArray, fillZero } from "./feature";
-import { Day } from "../models/date.model";
+import { CreateMonthArguments, Day, DayData } from "../models/date.model";
 /**
  * 转化成Date对象
  * @param date
@@ -82,29 +82,33 @@ function getEndMend(week: number, weekStart: number): number {
   const index: number = weeks.findIndex((t) => t === week);
   return weeks.filter((t, ti) => ti > index).length;
 }
+
+
 /**
  * 返回月份数据
  * @param date Date 对象 或 可被new Date对象解析;
- * @param weekStart number 周开始 0 - 6 ,默认1 从周一开始
+ * @param data 在对应日期中插入数据，以时间为key的对象；{'2022/01/01': {}}
+ * @param weekStart number 周开始 0 - 6 ,默认1 从周一开始; 0 代表星期日，1 代表星期一，2 代表星期二，依次类推。
  * @return Day[] 一维数组
  */
 function createMonth(
-  date: Date | number | string = Date.now(),
-  weekStart: number = 1
+  { date, data, weekStart, isSliceByWeek }: CreateMonthArguments
 ): Day[][] {
-  const newDate: Date = _date(date);
+  weekStart = weekStart ?? 1;
+  const newDate: Date = _date(date || Date.now());
   const weekEnd: number = weekStart - 1 < 0 ? 6 : weekStart - 1; // 可以优化
   const year: number = newDate.getFullYear();
   const month: number = newDate.getMonth(); // 当前月份的Date对象展示,非实际月份,实际月份需+1
   const currentMonth: number = month + 1; // 当前月份
   const nextMonth: number = month + 1; // 下一月份
   const prevMonth: number = month - 1; // 上一月份
+  const currentDay: number = newDate.getDate()
   const days: number = new Date(year, nextMonth, 0).getDate(); // 将月份下移到下一个月份，同时将日期设置为0；由于Date里的日期是1~31，所以Date对象自动跳转到上一个月的最后一天；getDate（）获取天数即可。
-  let res :Day[]= [];
+  let res: Day[] = [];
   for (let i = 1; i <= days; i++) {
     let day: number = i;
     let week: number = new Date(year, month, day).getDay(); // 0 - 6 ; 周日 - 周六
-
+    
     // 补足上月尾信息
     if (i === 1) {
       if (weekStart !== week) {
@@ -112,33 +116,35 @@ function createMonth(
         for (let _i = mendDays; _i > 0; _i--) {
           const prevDate = new Date(year, month, -_i + 1);
           const dateInfo = getYMDW(prevDate);
+          const _dateText = `${dateInfo.year}/${fillZero(dateInfo.month)}/${fillZero(
+            dateInfo.day
+          )}`
           res.push({
-            date: prevDate,
-            data: {
-              text: `${dateInfo.year}/${fillZero(dateInfo.month)}/${fillZero(
-                dateInfo.day
-              )}`,
-              current: false,
-              week: dateInfo.week,
-              year: dateInfo.year,
-              month: dateInfo.month,
-              day: dateInfo.day,
-            },
+            source: prevDate,
+            text:_dateText ,
+            current: false,
+            week: dateInfo.week,
+            year: dateInfo.year,
+            month: dateInfo.month,
+            day: dateInfo.day,
+            isToday: false, 
+            data: data?.[_dateText]
           });
         }
       }
     }
 
+    const _currentDateText= `${year}/${fillZero(currentMonth)}/${fillZero(day)}`
     res.push({
-      date: new Date(year, month, day),
-      data: {
-        current: true,
-        text: `${year}/${fillZero(currentMonth)}/${fillZero(day)}`,
-        week,
-        day,
-        month,
-        year,
-      },
+      current: true,
+      text:_currentDateText ,
+      isToday: day === currentDay,
+      source: new Date(year, month, day),
+      data: data?.[_currentDateText],
+      week,
+      day,
+      month,
+      year,
     });
 
     // 补足下月首信息
@@ -148,26 +154,26 @@ function createMonth(
         for (let _i = 0; _i < mendDays; _i++) {
           const nextDate = new Date(year, nextMonth, _i + 1);
           const dateInfo = getYMDW(nextDate);
+          const  _dateText = `${dateInfo.year}/${fillZero(dateInfo.month)}/${fillZero(
+            dateInfo.day
+          )}`
           res.push({
-            date: nextDate,
-            data: {
-              text: `${dateInfo.year}/${fillZero(dateInfo.month)}/${fillZero(
-                dateInfo.day
-              )}`,
-              current: false,
-              week: dateInfo.week,
-              year: dateInfo.year,
-              month: dateInfo.month,
-              day: dateInfo.day,
-            },
+            source: nextDate,
+            text: _dateText,
+            current: false,
+            data: data?.[_dateText],
+            week: dateInfo.week,
+            year: dateInfo.year,
+            month: dateInfo.month,
+            day: dateInfo.day,
+            isToday: false,
           });
         }
       }
     }
   }
-
- 
   return sliceArray(res, 7)
 }
+ 
 
 export { createMonth };
--- src/models/date.model.ts
+++ src/models/date.model.ts
@@ -1,11 +1,31 @@
+/*
+ * @Author: q.chen.work q.chen.work@outlook.com
+ * @Date: 2023-01-12 11:18:45
+ * @LastEditors: q.chen.work q.chen.work@outlook.com
+ * @LastEditTime: 2023-01-13 10:58:30
+ * @FilePath: /Utils/src/models/date.model.ts
+ * @Description: 
+ * 
+ * Copyright (c) 2023 by q.chen.work q.chen.work@outlook.com, All Rights Reserved. 
+ */
 export interface Day {
-  date: Date;
-  data: {
-    year: number;
-    month: number;
-    day: number;
-    text: string; // 当前日期 2020/10/10
-    week: number; // 0-6
-    current: boolean; // 是否当前月
-  };
+  year: number;
+  month: number;
+  day: number;
+  text: string; // 当前日期 2020/10/10
+  week: number; // 0-6
+  current: boolean; // 是否当前月
+  isToday: boolean; // 是否今天
+  source: Date;
+  data?: any; // 补足数据
+}
+export interface DayData {
+  [key: string]: any
+}
+
+export interface CreateMonthArguments {
+  date: Date | number | string;  // 初始日期
+  data?: DayData; // 补足到对应日期的数据
+  weekStart: number; // 日期开始周，默认为1 从周一开始
+  isSliceByWeek?: boolean; // 是否按周切割，默认不切割
 }
