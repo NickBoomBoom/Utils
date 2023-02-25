da9d20e476b8328f2177f3034356c93dfe61c3ae
q.chen <q.chen.self@outlook.com>
2023-02-26 00:57:54 +0800

    update

--- build.ts
+++
@@ -1,3 +0,0 @@
-import fs from 'fs'
-fs.copyFileSync('./package.json', './dist/package.json')
-fs.copyFileSync('./README.md', './dist/README.md')
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
- 
-export  * from './core/WeChat'
-export  * from './core/dom'
-export  * from './core/feature'
-export  * from './core/platform'
-export  * from './core/storage'
-export  * from './core/date'
-export  * from './core/bom'
-export  * from './core/VueHistory';
-export  *  from 'decimal.js' 
+
+export * from './core/WeChat'
+export * from './core/dom'
+export * from './core/feature'
+export * from './core/platform'
+export * from './core/storage'
+export * from './core/date'
+export * from './core/bom'
+export * from './core/VueHistory';
+export * from 'decimal.js' 
